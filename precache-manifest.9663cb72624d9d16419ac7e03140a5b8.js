0de35e18b0dd679a6b204f9cc9cf1cf81a201342
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-10-17 21:42:31 -0600

    Updates

---
+++ precache-manifest.9663cb72624d9d16419ac7e03140a5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb22e1f5fd686d7e80b5a1352d8dec0",
+    "url": "/Where-In-The-World/index.html"
+  },
+  {
+    "revision": "acc58ab29818e445e2bd",
+    "url": "/Where-In-The-World/static/css/main.be2d34a0.chunk.css"
+  },
+  {
+    "revision": "0653d13e1a358e19867b",
+    "url": "/Where-In-The-World/static/js/2.5e5aebb9.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/Where-In-The-World/static/js/2.5e5aebb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc58ab29818e445e2bd",
+    "url": "/Where-In-The-World/static/js/main.b916c609.chunk.js"
+  },
+  {
+    "revision": "4da2c13d29ca29f14395",
+    "url": "/Where-In-The-World/static/js/runtime-main.5e14e87f.js"
+  }
+]);
